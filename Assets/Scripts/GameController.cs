4770721565f50933055f68943833c1800b8d2013
DESKTOP-4HPAV50\ASUS <andreas1439setiawan@gmail.com>
2021-09-22 17:52:09 +0700

    Upload Tugas Chapter 5

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    private Bird _shotBird;
+    public BoxCollider2D TapCollider;
+
+    private bool _isGameEnded;
+
+    [SerializeField] private GameObject _panel;
+    [SerializeField] private Text _statusInfo;
+
+    public Button RestartButton, NextLevelButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RestartButton.onClick.AddListener(Restart);
+        NextLevelButton.onClick.AddListener(Next);
+
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (_isGameEnded)
+        {
+            return;
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+        else
+        {
+            SetGameOver(false);
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            SetGameOver(true);
+        }
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    void OnMouseUp()
+    {
+        if(_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+    public void SetGameOver(bool isWin)
+    {
+        _isGameEnded = true;
+
+        _statusInfo.text = isWin ? "YOU WIN!" : "YOU LOSE!";
+        _panel.gameObject.SetActive(true);
+
+    }
+}
---
+++ Assets/Scripts/Owl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+    [SerializeField]
+    public float _explosionRange;
+    public float _explosionForce;
+    public LayerMask _affectedLayer;
+
+    void explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, _explosionRange, _affectedLayer);
+     
+            foreach (Collider2D obj in objects)
+            {
+                Vector2 direction = obj.transform.position - transform.position;
+                obj.GetComponent<Rigidbody2D>().AddForce(direction * _explosionForce);
+            }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        explode();
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _explosionRange);
+    }
+}
